ffc6b3fb4945a609b5a6123e97577c9090c7a43c
Amit Dhir <sahil.isfine@gmail.com>
2024-05-15 17:48:59 -0700

    adding videoplayer

---
+++ videoplayer.py
@@ -0,0 +1,22 @@
+import vlc
+import threading
+def play_video(filepath : str) -> None:
+    instance = vlc.Instance()
+    player = instance.media_player_new()
+    media = instance.media_new(filepath)
+    player.set_media(media)
+
+    media_end_event = threading.Event()
+
+    # sets flag ending program
+    def end_callback(event):
+        print("Media finished playing")
+        media_end_event.set()
+
+    events = player.event_manager()
+    events.event_attach(vlc.EventType.MediaPlayerEndReached, end_callback)
+
+    player.play()
+
+    media_end_event.wait()
+
